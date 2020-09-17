77adcf1a44ee89cdb96f12ec78dcff1391f6f6ba
Anthony Bokor <anthony.bokor@my.jcu.edu.au>
2020-09-17 23:15:19 +1000

    Fixed programming_languags and made languages.py file

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -21,24 +21,3 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
-
-
-def run_tests():
-    java = ProgrammingLanguage("Java", "Static", True, 1995)
-    c = ProgrammingLanguage("C++", "Static", False, 1983)
-    # c is the programming language C++
-    # THe "++" is not added due to it causing errors
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    languages = [java, c, python, visual_basic, ruby]
-    for language in languages:
-        print(language)
-    print("The dynamically typed languages include:")
-    for language in languages:
-        if language.is_dynamic():
-            print(language.name)
-
-
-if __name__ == "__main__":
-    run_tests()
